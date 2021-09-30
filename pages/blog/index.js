0aa9260fe20fa5030cd7f0119f3a3d8791ba93ae
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-09-30 15:19:01 +0100

    add search feature via SWR

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,31 @@
-import { getPosts } from "../../lib/data";
 import Link from "next/dist/client/link";
+import { useState } from "react";
+import { request } from "graphql-request";
+import useSWR from "swr";
+
+const fetcher = (endpoint, query, variables) => request(endpoint, query, variables);
 
 export const getStaticProps = async () => {
-  const data = await getPosts();
+  const data = await fetcher(
+    process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
+    ` {
+    posts(first: 3, orderBy: date_DESC) {
+      title
+      slug
+      description
+      date
+      tags
+      author {
+        name
+        image {
+          url
+          width
+          height
+        }
+      }
+    }
+  }`,
+  );
   return {
     props: {
       posts: data.posts,
@@ -11,11 +34,49 @@ export const getStaticProps = async () => {
 };
 
 export default function BlogPage({ posts }) {
-  console.log("Blogpage data:", posts);
+  const [searchValue, setSearchValue] = useState("");
+  const { data, error } = useSWR(
+    [
+      process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
+      `query getPosts($searchValue:String) {
+        posts(orderBy: date_DESC, where:{title_contains: $searchValue}){
+          id
+          title
+          date
+          slug
+          description
+          author{
+            name
+          }    
+        }
+      }`,
+      searchValue,
+    ],
+    (endpoint, query) => fetcher(endpoint, query, { searchValue }),
+    { initialData: posts, revalidationOnFocus: true },
+  );
+
+  if (error) {
+    return (
+      <div>
+        <h2>There was an error with data fetching</h2>
+      </div>
+    );
+  }
   return (
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-0">
-      <div className="mt-20">
-        {posts?.map((post) => (
+      <h1 className="text-5xl text-gray-600 font-serif mb-6 font-bold">The Blog</h1>
+      <div>
+        <input
+          type="text"
+          value={searchValue}
+          placeholder="Search Blog posts"
+          className="foucs:outline-none mb-6 focus:ring-2 focus:ring-gray-900 w-full rounded-lg border h-10 pl-4 text-lg text-gray-800  border-gray-200"
+          onChange={(event) => setSearchValue(event.target.value)}
+        />
+      </div>
+      <div className="">
+        {data?.posts.map((post) => (
           <div key={post.slug} className="grid md:grid-cols-4 py-6">
             <div className="mb-2 md:mb-0 md:col-span-1" key={post.slug}>
               <p className="text-gray-600 tx-sm">{new Date(post.date).toDateString()}</p>
