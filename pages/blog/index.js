3022503e82159475ae4a4704ef6ef106ba670504
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-09-30 16:52:16 +0100

    adding pagination

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,52 +8,71 @@ const fetcher = (endpoint, query, variables) => request(endpoint, query, variabl
 export const getStaticProps = async () => {
   const data = await fetcher(
     process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
-    ` {
-    posts(first: 3, orderBy: date_DESC) {
-      title
-      slug
-      description
-      date
-      tags
-      author {
-        name
-        image {
-          url
-          width
-          height
+    `query getPosts($searchValue: String) {
+      postsConnection(orderBy: date_DESC, where: {title_contains: $searchValue}, first: 2, skip: 0) {
+        edges {
+          node {
+            id
+            title
+            date
+            slug
+            description
+            id
+            author {
+              name
+            }
+          }
+        }
+        pageInfo{
+          hasNextPage
+          hasPreviousPage
+          pageSize
         }
       }
-    }
-  }`,
+    }`,
   );
+  console.log("fetc", data);
   return {
     props: {
-      posts: data.posts,
+      posts: data,
     },
   };
 };
 
 export default function BlogPage({ posts }) {
   const [searchValue, setSearchValue] = useState("");
+  const [skip, setSkip] = useState(0);
   const { data, error } = useSWR(
     [
       process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
-      `query getPosts($searchValue:String) {
-        posts(orderBy: date_DESC, where:{title_contains: $searchValue}){
-          id
-          title
-          date
-          slug
-          description
-          author{
-            name
-          }    
-        }
-      }`,
+      `query getPosts($searchValue: String $skip: Int) {
+        postsConnection(orderBy: date_DESC, where:{title_contains: $searchValue}, first: 2, skip: $skip){
+           edges {
+            node {
+              id
+              title
+              date
+              slug
+              description
+              author{
+                name
+              } 
+            }
+          }  
+          pageInfo{
+            hasNextPage
+            hasPreviousPage
+            pageSize
+          }
+       }
+      }
+      `,
       searchValue,
+      skip,
     ],
-    (endpoint, query) => fetcher(endpoint, query, { searchValue }),
-    { initialData: posts, revalidationOnFocus: true },
+
+    (endpoint, query) => fetcher(endpoint, query, { searchValue, skip }),
+    { initialData: posts, revalidateOnFocus: true },
   );
 
   if (error) {
@@ -75,23 +94,49 @@ export default function BlogPage({ posts }) {
           onChange={(event) => setSearchValue(event.target.value)}
         />
       </div>
-      <div className="">
-        {data?.posts.map((post) => (
-          <div key={post.slug} className="grid md:grid-cols-4 py-6">
-            <div className="mb-2 md:mb-0 md:col-span-1" key={post.slug}>
-              <p className="text-gray-600 tx-sm">{new Date(post.date).toDateString()}</p>
+
+      <div>
+        {data?.postsConnection?.edges?.map((post) => (
+          <div key={post.node.slug} className="grid md:grid-cols-4 py-6">
+            <div className="mb-2 md:mb-0 md:col-span-1" key={post.node.slug}>
+              <p className="text-gray-600 tx-sm">{new Date(post.node.date).toDateString()}</p>
             </div>
             <div className="md:col-span-3">
-              <Link key={post.slug} href={`/blog/${post.slug}`}>
+              <Link key={post.node.slug} href={`/blog/${post.node.slug}`}>
                 <a className="text-2xl font-semibold text-gray-900 hover:text-gray-700 transition-colors duration-300">
-                  {post.title}
+                  {post.node.title}
                 </a>
               </Link>
-              <p className="text-gray-700 leading-relaxed">{post.description}</p>
-              <div className="text-sm text-gray-900 font-semibold">{post.author.name}</div>
+              <p className="text-gray-700 leading-relaxed">{post.node.description}</p>
+              <div className="text-sm text-gray-900 font-semibold">{post.node.author.name}</div>
             </div>
           </div>
         ))}
+      </div>
+      <div className="flex space-x-5 justify-center items-center ">
+        <div>
+          <button
+            onClick={() => {
+              setSkip(skip - 2);
+            }}
+            disabled={!data?.postsConnection?.pageInfo?.hasPreviousPage}
+            className="bg-indigo-700 w-20 text-white px-3 py-1 rounded-md disabled:bg-gray-400 disabled:text-gray-800">
+            Previous
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setSkip(skip + 2);
+            }}
+            disabled={!data?.postsConnection?.pageInfo?.hasNextPage}
+            className="bg-indigo-700 w-20 text-white px-3 py-1 rounded-md disabled:bg-gray-400 disabled:text-gray-800">
+            Next
+          </button>
+        </div>
+        <div className="text-gray-700">
+          Total Pages : {data?.postsConnection?.pageInfo?.pageSize}
+        </div>
       </div>
     </div>
   );
